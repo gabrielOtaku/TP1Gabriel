ddf7b7ca1c427effa67d750f4d5b883dd9738aa4
Gabriel Herve <81375156+gabrielOtaku@users.noreply.github.com>
2026-09-17 11:22:20 -0400

    Mise en forme Layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,20 @@
-import { Stack } from "expo-router";
+import { couleurs } from "@/theme/couleurs";
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+
+  return(
+    <Tabs
+    screenOptions={{
+      headerStyle : {borderBlockColor: couleurs.Primaire},
+      headerTintColor: couleurs.Secondaire,
+      tabBarStyle: {backgroundColor: couleurs.Secondaire, borderTopWidth:0},
+      tabBarActiveTintColor: couleurs.Primaire,
+      tabBarInactiveTintColor: couleurs.Inactif
+
+    }}>
+    </Tabs>
+  );
+
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,9 @@
-import { Text, View, StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Text>Acceuil</Text>
     </View>
   );
 }
@@ -11,7 +11,7 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });

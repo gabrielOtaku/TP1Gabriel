0d499cf6f1856b5f90ba5b89f9009198cf0100ee
Gabriel Herve <81375156+gabrielOtaku@users.noreply.github.com>
2026-09-17 11:30:40 -0400

    footer

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,20 +1,43 @@
-import { couleurs } from "@/theme/couleurs";
-import { Ionicons } from "@expo/vector-icons";
-import { Tabs } from "expo-router";
+import { couleurs } from '@/theme/couleurs';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { Tabs } from 'expo-router';
 
 export default function RootLayout() {
-
-  return(
+  return (
     <Tabs
-    screenOptions={{
-      headerStyle : {borderBlockColor: couleurs.Primaire},
-      headerTintColor: couleurs.Secondaire,
-      tabBarStyle: {backgroundColor: couleurs.Secondaire, borderTopWidth:0},
-      tabBarActiveTintColor: couleurs.Primaire,
-      tabBarInactiveTintColor: couleurs.Inactif
-
-    }}>
+      screenOptions={{
+        headerStyle: { borderBlockColor: couleurs.Primaire },
+        headerTintColor: couleurs.Secondaire,
+        tabBarStyle: {
+          backgroundColor: couleurs.Secondaire,
+          borderTopWidth: 0,
+        },
+        tabBarActiveTintColor: couleurs.Primaire,
+        tabBarInactiveTintColor: couleurs.Inactif,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Films',
+          tabBarIcon: ({ color, size }) => <MaterialIcons name="movie" size={size} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Casting',
+          tabBarIcon: ({ color, size }) => <AntDesign name="team" size={size} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Favoris',
+          tabBarIcon: ({ color, size }) => <MaterialIcons name="star" size={size} color={color} />,
+        }}
+      />
     </Tabs>
   );
-
 }
